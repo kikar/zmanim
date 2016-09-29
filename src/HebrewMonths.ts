e41e1eef8d17b7b77cc06ceee0441f6344971bb6
Gady Piazza <gady90@gmail.com>
2016-09-29 22:52:30 +0100

    Add months constants

---
+++ src/HebrewMonths.ts
@@ -0,0 +1,15 @@
+export enum months {
+    NISSAN = 1,
+    IYAR,
+    SIVAN,
+    TAMMUZ,
+    AV,
+    ELUL,
+    TISHREI,
+    CHESVAN,
+    KISLEV,
+    TEVET,
+    SHEVAT,
+    ADAR_I,
+    ADAR_II
+}
--- test/greeter-spec.ts
+++ test/greeter-spec.ts
@@ -1,12 +1,17 @@
 
-import { Greeter } from "../src/greeter";
+import { months } from '../src/HebrewMonths';
 import * as chai from "chai";
 
 const expect = chai.expect;
 
-describe("greeter", () => {
-  it("should greet with message", () => {
-    const greeter = new Greeter("friend");
-    expect(greeter.greet()).to.equal("Bonjour, friend!");
+describe('months', () => {
+  it('should pair months with numbers', () => {
+    expect(months.ADAR_I).to.equal(12);
+    expect(months.NISSAN).to.equal(1);
   });
-});
+
+  it('should pair numbers with months', () => {
+    expect(months[7]).to.equal('TISHREI');
+    expect(months[9]).to.equal('KISLEV');
+  });
+})
